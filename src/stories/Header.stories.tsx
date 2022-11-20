7ca354bea15391e29d7b43f62d4ab0c63981e460
Somayeh.Ghanavati <s.ghanavati94@gmail.com>
2022-11-20 17:36:12 +0330

    add decorators

---
+++ src/stories/Header.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { Header } from '../Component/Header/Header'
+
+export default {
+  title: 'Component/Header',
+  component: Header,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'padded',
+  },
+} as ComponentMeta<typeof Header>
+
+const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />
+
+export const LoggedIn = Template.bind({})
+LoggedIn.args = {
+  user: {
+    name: 'Jane Doe',
+  },
+}
+
+export const LoggedOut = Template.bind({})
+LoggedOut.args = {}
